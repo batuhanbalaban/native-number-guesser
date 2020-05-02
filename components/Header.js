79a7091ccbd756e996e4d11a8123dd77d33804d3
Batuhan Balaban <batuhan.balaban@yahoo.com>
2020-05-02 23:57:37 +0100

    Platform dependent files and Platform.select

--- components/Header.js
+++ components/Header.js
@@ -8,14 +8,14 @@ import TextTitle from './TextTitle';
 const Header = props => {
 
     return (
-        <View style={styles.header}>
+        <View style={{ ...styles.headerBase, ...Platform.select({ ios: styles.headerIos, android: styles.headerAndroid }) }}>
             <TextTitle style={styles.headerTitle}>{props.title}</TextTitle>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase: {
         width: '100%',
         height: 90,
         paddingTop: 36,
@@ -24,6 +24,14 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
         borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
+    },
+    headerAndroid: {
+        backgroundColor: Colors.primary,
+    },
+    headerIos: {
+        backgroundColor: 'white',
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
     },
     headerTitle: {
         color: Platform.OS === 'ios' ? Colors.primary : 'white',
